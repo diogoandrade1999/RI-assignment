e0a700ede2c78ca9c180447d8ce5f889961de8ab
diogoandrade1999 <diogo.andrade@ua.pt>
2020-10-21 22:44:13 +0100

    review the code

--- CorpusReader.py
+++ CorpusReader.py
@@ -1,26 +1,17 @@
 from csv import reader
-import logging
-
-logging.basicConfig(
-	level=logging.INFO, format="%(message)s"
-)
-
-logger = logging.getLogger("CorpusReader")
 
 
 class CorpusReader:
 	def __init__(self, path):
-		self.path = path
-		self.proc_dict = {}
+		self._path = path
 
 	def process(self):
-		with open(self.path, "r") as filereader:
+		proc_dict = {}
+		with open(self._path, "r") as filereader:
 			filereader.readline()
 			counter_id = 0
 			for line in reader(filereader):
-				if line[2] == "" or line[7] == "":
-					continue
-
-				self.proc_dict[counter_id] = (line[2], line[7])
-				counter_id += 1
-		return self.proc_dict
+				if line[2] != "" and line[7] != "":
+					proc_dict[counter_id] = (line[2], line[7])
+					counter_id += 1
+		return proc_dict
--- Tokenizer.py
+++ Tokenizer.py
@@ -1,5 +1,4 @@
 import abc
-import logging
 import re
 from CorpusReader import CorpusReader
 import json
@@ -10,38 +9,50 @@ class Tokenizer(metaclass=abc.ABCMeta):
 	def tokenize(self):
 		pass
 
+	@property    
+	@abc.abstractmethod
+	def tokens(self):
+		pass
+
 
 class SimpleTokenizer(Tokenizer):
 	def __init__(self, file):
-		self.corpus = CorpusReader(file)
-		self.tokens = []
-		self.index = []
+		self._corpus = CorpusReader(file)
+		self._tokens = []
+		self._index = []
+
+	@property
+	def tokens(self):
+		return self._tokens
 
 	def tokenize(self):
-		processed_files = self.corpus.process()
+		processed_files = self._corpus.process()
 		for file_id, doc in processed_files.items():
 			# replaces all non-alphabetic characters by a space
 			tokens = re.sub('[^a-zA-Z]+', ' ', doc[0] + doc[1])
 			# put token in lowercase
 			tokens = tokens.lower()
 			# ignores all tokens with less than 3 characters
-			self.tokens += [(token, file_id) for token in tokens.split() if len(token) >= 3]
+			self._tokens += [(token, file_id) for token in tokens.split() if len(token) >= 3]
 
 
 class ImprovedTokenizer(Tokenizer):
 	def __init__(self, file):
-		self.corpus = CorpusReader(file)
-		self.tokens = []
+		self._corpus = CorpusReader(file)
+		self._tokens = []
 		with open("stopwords.json", "r") as stop:
-			self.stopwords = json.load(stop)
+			self._stopwords = json.load(stop)
+
+	@property
+	def tokens(self):
+		return self._tokens
 
 	def tokenize(self):
-		processed_files = self.corpus.process()
+		processed_files = self._corpus.process()
 		for file_id, doc in processed_files.items():
 			# replaces all non-alphabetic characters by a space
 			tokens = re.sub('[^a-zA-Z]+', ' ', doc[0] + doc[1])
 			# put token in lowercase
 			tokens = tokens.lower()
 			# ignores all tokens with less than 3 characters
-			self.tokens += [(token, file_id) for token in tokens.split() if token not in self.stopwords]
-
+			self._tokens += [(token, file_id) for token in tokens.split() if token not in self._stopwords]
