ad2a1279def8c89dbafe80d400f98f315a6fbfcd
Diogo Andrade <39158546+diogoandrade1999@users.noreply.github.com>
2020-10-26 16:00:02 +0000

    Update CorpusReader.py

--- CorpusReader.py
+++ CorpusReader.py
@@ -7,11 +7,9 @@ class CorpusReader:
 
 	def process(self):
 		proc_dict = {}
-		doc_id = 0
 		with open(self._path, "r") as filereader:
 			filereader.readline()
-			for line in reader(filereader):
+			for doc_id, line in enumerate(reader(filereader)):
 				if line[2] != "" and line[7] != "":
 					proc_dict[doc_id] = line[2] + " " + line[7]
-					doc_id += 1
 		return proc_dict
