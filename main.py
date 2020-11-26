830497219c0a0978e0fb2450f306e498b3bf4442
diogoandrade1999 <diogo.andrade@ua.pt>
2020-11-26 16:04:47 +0000

    metrics

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def questions(indexer:Indexer) -> None:
 
 
 def metrics(query_reader:QueryReader, indexer:Indexer, tokenizer:Tokenizer, use_bm:bool) -> None:
-    resulsts = {}
+    results = {}
     for query_number, query in query_reader.queries.items():
         start_time = time.time()
 
@@ -62,43 +62,57 @@ def metrics(query_reader:QueryReader, indexer:Indexer, tokenizer:Tokenizer, use_
         docs_relevance = query_reader.queries_relevance[query_number][1].union(
                             query_reader.queries_relevance[query_number][2])
         docs_retrieved = set(docs.keys())
-        docs_relevance_retrieved = docs_retrieved & docs_relevance
 
-        num_docs_relevance = len(docs_relevance)
-        num_docs_retrieved = len(docs_retrieved)
-        num_docs_relevance_retrieved = len(docs_relevance_retrieved)
-        
-        precision = 0
-        recall = 0
-        f_measure = 0
-        average_precision = 0
-        dcg = 0
-        ndcg = 0
+        results[query_number] = {}
+        results[query_number]['latency'] = latency
+        for x in [10, 20, 50]:
+            docs_retrieved = set(list(docs_retrieved)[:x])
 
-        if num_docs_relevance_retrieved != 0:
-            if num_docs_retrieved != 0:
-                precision = num_docs_relevance_retrieved / num_docs_retrieved
+            docs_relevance_retrieved = docs_retrieved & docs_relevance
 
-            if num_docs_relevance != 0:
-                recall = num_docs_relevance_retrieved / num_docs_relevance
+            num_docs_relevance = len(docs_relevance)
+            num_docs_retrieved = len(docs_retrieved)
+            num_docs_relevance_retrieved = len(docs_relevance_retrieved)
 
-            if (precision + recall) != 0:
-                f_measure = (2 * precision * recall) / (precision + recall)
+            precision = 0
+            recall = 0
+            f_measure = 0
+            average_precision = 0
+            ndcg = 0
 
-            # está mal
-            docs_precision = [docs[doc] for doc in docs_relevance_retrieved]
-            average_precision = sum(docs_precision) / num_docs_relevance_retrieved
+            if num_docs_relevance_retrieved != 0:
+                if num_docs_retrieved != 0:
+                    precision = num_docs_relevance_retrieved / num_docs_retrieved
 
+                if num_docs_relevance != 0:
+                    recall = num_docs_relevance_retrieved / num_docs_relevance
 
-        resulsts[query_number] = (precision, recall, f_measure, average_precision, ndcg, latency)
-        
-        # if query_number == '1':
-        #     break
+                if (precision + recall) != 0:
+                    f_measure = (2 * precision * recall) / (precision + recall)
 
-    logger.info(' #  Precision Recall    F-measure Average Precision NDCG      Latency')
-    for query_number, values in resulsts.items():
-        precision, recall, f_measure, average_precision, ndcg, latency = values
-        logger.info('%2s %9f %9f %9f %9f %17f %9f' % (query_number, precision, recall, f_measure, average_precision, ndcg, latency))
+                # está mal
+                docs_precision = [docs[doc] for doc in docs_relevance_retrieved]
+                average_precision = sum(docs_precision) / num_docs_relevance_retrieved
+
+                dcg = 0
+            results[query_number][x] = (precision, recall, f_measure, average_precision, ndcg)
+
+    logger.info(' # %25s %25s %25s %30s %25s %25s' % ('Precision', 'Recall', 'F-measure', 'Average Precision', 'NDCG', 'Latency'))
+    logger.info('   %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s %9s' % \
+        ('@10', '@20', '@30', '@10', '@20', '@30', '@10', '@20', '@30', '@10', '@20', '@30', '@10', '@20', '@30'))
+    for query_number, x in results.items():
+        precision1, recall1, f_measure1, average_precision1, ndcg1 = x[10]
+        precision2, recall2, f_measure2, average_precision2, ndcg2 = x[20]
+        precision3, recall3, f_measure3, average_precision3, ndcg3 = x[50]
+        latency = x['latency']
+        logger.info('%2s %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f %9f' % \
+            (query_number,
+            precision1, precision2, precision3,
+            recall1, recall2, recall3,
+            f_measure1, f_measure2, f_measure3,
+            average_precision1, average_precision2, average_precision3,
+            ndcg1, ndcg2, ndcg3,
+            latency))
 
 
 def main(
@@ -125,9 +139,7 @@ def main(
 
     # create indexer
     if use_bm:
-        k1 = bm_k1 if bm_k1 else 1.2
-        b = bm_b if bm_b else 0.75
-        indexer = IndexerBM25(corpus, tokenizer, k1, b)
+        indexer = IndexerBM25(corpus, tokenizer, bm_k1, bm_b)
     else:
         indexer = Indexer(corpus, tokenizer)   
 
@@ -163,18 +175,17 @@ if __name__ == "__main__":
     parser.add_argument("-t", dest="improved_tokenizer", required=False, help="Use improved tokenizer", default=False, action='store_true')
     parser.add_argument("-w", dest="indexer_file", required=False, help="Write index to file", default=None)
     parser.add_argument("-b", dest="bm25", required=False, help="Use the BM25 method to rank", default=False, action='store_true')
-    parser.add_argument("--bk1", dest="bm25_k1_value", required=False, help="K value for the BM25 method", type=float)
-    parser.add_argument("--bb", dest="bm25_b_value", required=False, help="B value for the BM25 method", type=float)  
+    parser.add_argument("--bk1", dest="bm25_k1_value", required=False, help="K value for the BM25 method", type=float, default=1.2)
+    parser.add_argument("--bb", dest="bm25_b_value", required=False, help="B value for the BM25 method", type=float, default=0.75)  
     parser.add_argument("-q", dest="query_file_path", required=True, help="Queries file path")
     parser.add_argument("-qr", dest="query_relevance_file_path", required=True, help="Queries relevance file path")
-
     args = parser.parse_args()
 
-    if not args.bm25 and (args.bm25_k1_value is not None or args.bm25_b_value is not None):
+    if not args.bm25 and (args.bm25_k1_value != 1.2 or args.bm25_b_value != 0.75):
         parser.error("--bk1 and --bb requires the flag -b")
-    elif args.bm25_k1_value is not None and not (1 < args.bm25_k1_value < 2):
+    elif args.bm25_k1_value != 1.2 and not (1 < args.bm25_k1_value < 2):
         parser.error("K value for the BM25 method must be greater than 1 and less than 2")
-    elif args.bm25_b_value is not None and not (0 < args.bm25_b_value < 1):
+    elif args.bm25_b_value != 0.75 and not (0 < args.bm25_b_value < 1):
         parser.error("B value for the BM25 method must be greater than 0 and less than 1")
 
     main(args.data_file_path,
